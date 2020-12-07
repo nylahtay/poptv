6522f1797e8ae262d2ee5656fd200f422847ab45
Nylah Tay <6609234+nylahtay@users.noreply.github.com>
2020-12-06 23:49:00 -0600

    added components and test data

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header theTitle="Welcome to Your Vue.js App"/>
+    <div v-for="show in shows" v-bind:key="show.id" class="card-group">
+      <Card v-bind:theShow="show"/>
+    </div>
   </div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Card from './components/Card.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default {
   name: 'App',
   components: {
-    //HelloWorld
+    Header,
+    Card
+  },
+  data()
+  {
+    return {
+      shows: [
+        {id:1, name:"movie 1", overview: "this is the details of the item 1. Lorum ispms iah aisSd aisdha ankaseihasdklwsej isiadh ihs iais id i haiiha keihab ooak", poster_path: ""},
+        {id:2, name:"The big movie 2", overview: "details of the item 2. Lorum ispmaseihasdklwsej isiadh ihs iais id i h", poster_path: ""}
+      ]
+    }
+  },
+  mounted()
+  {
+    
   }
 }
 </script>
